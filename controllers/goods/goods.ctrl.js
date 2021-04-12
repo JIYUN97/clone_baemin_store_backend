a094a77f860e944f7ab8ba1fa05cfe51b652229f
kimjinsoo <1004kjs_@naver.com>
2021-04-13 00:13:17 +0900

    goods router 생성

--- controllers/goods/goods.ctrl.js
+++ controllers/goods/goods.ctrl.js
@@ -1,2 +1,31 @@
+const Goods = require('../../models/goods')
+
+// 메인 페이지
+exports.get_main_page = async(req, res) => {
+    goods = await Goods.find({})
+    res.status(200).send({
+        "result" : "success",
+        "goods"  : goods
+    })
+}
+
+// 카테고리 페이지
+exports.get_category_page = async(req, res) => {
+    console.log(req.params.cateogoryId)
+    category_goods = await Goods.find({
+        category
+    })
+}
+
+// 상세 페이지
+exports.get_detail_page = async(req, res) => {
+    res.send("상세페이지 연결 성공")
+}
+
+// 검색 
+// exports.get_search = (req, res) => {
+//     res.send("검색완료")
+//     console.log(req.query)
+// }
 
 

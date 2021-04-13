de3438d7f374bf036c962d8a03141d8005b8b269
kimjinsoo <1004kjs_@naver.com>
2021-04-13 13:41:46 +0900

    상세페이지 메인페이지 검색 생성

--- controllers/goods/goods.ctrl.js
+++ controllers/goods/goods.ctrl.js
@@ -1,39 +1,67 @@
-const Goods = require('../../models/goods')
+const { Goods, Category } = require('../../models')
+const escapeStringRegexp  = require('escape-string-regexp');
 
 // 메인 페이지
 exports.get_main_page = async(req, res) => {
-    goods = await Goods.find({})
-    res.status(200).send({
-        "result" : "success",
-        "goods"  : goods
-    })
+    try{
+        goods = await Goods.find({})
+        res.status(200).send({
+            "result" : "success",
+            "goods"  : goods
+        })
+    }catch(err) {
+        res.status(400).send({"result" : "fail"})
+    }
 }
 
 // 카테고리 페이지
-// exports.get_category_page = async(req, res) => {
-//     categoryId = req.params.cateogoryId
+exports.get_category_page = async(req, res) => {
+    try {
+        categoryId = req.params.cateogoryId
+        category   = await Category.find({
+            "_id" : categoryId
+        })
+        
+        category_goods = await Goods.find({category : category.name})
 
-    
-
-//     category_goods = await Goods.find({
-//         categoryId : categoryId
-//     })
-// }
+        res.status(200).send({
+            "result" : "success",
+            "goods"  : category_goods
+        })
+    }catch(err) {
+        res.status(400).send({"result" : "fail"})
+    }
+}
 
 // 상세 페이지
 exports.get_detail_page = async(req, res) => {
-    goodsId = req.params.goodsId
-    goods   = await Goods.find({ "_id" : goodsId})
-    res.status(200).send({
-        "result" : "success",
-        "goods"  : goods
-    })
+    try {
+        goodsId = req.params.goodsId
+        goods   = await Goods.find({ "_id" : goodsId})
+        res.status(200).send({
+            "result" : "success",
+            "goods"  : goods
+        })
+    }catch(err) {
+        
+        res.status(400).send({"result" : "fail"})
+    }
 }
 
 // 검색 
-// exports.get_search = (req, res) => {
-//     res.send("검색완료")
-//     console.log(req.query)
-// }
+exports.search = async(req, res) => {
+    try {
+        const keyword = req.query.keyword
+        const goods  = await Goods.find({ title : {'$regex' : keyword}})
+        
+        res.status(200).send({
+            "result" : "success",
+            "goods"  : goods 
+        })
+    }catch(err) {
+        console.log(err)
+        res.status(400).send({"result" : "fail"})
+    }
+}
 
 
--- controllers/goods/index.js
+++ controllers/goods/index.js
@@ -8,10 +8,13 @@ router.get('/',ctrl.get_main_page);
 // 카테고리 페이지
 router.get('/category/:cateogoryId', ctrl.get_category_page);
 
+// 검색 기능
+router.get('/goods_search', ctrl.search)
+
 // 상세페이지
 router.get('/:goodsId', ctrl.get_detail_page)
 
 // 검색 기능
-// router.get('/goods_search', ctrl.get_search)
+router.get('/goods_search', ctrl.get_search)
 
 module.exports = router;

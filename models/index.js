8f879a186e9d9ffd13bdd1b50d585ca070591db9
YOOJIYUN <ys7638@naver.com>
2021-04-13 11:19:23 +0900

    fix models/index.js

--- models/index.js
+++ models/index.js
@@ -3,5 +3,6 @@ DB.User = require("./user");
 DB.Goods = require("./goods");
 DB.Order = require("./order");
 DB.Comment = require("./comment");
+DB.Category = require("./category");
 
 module.exports = DB;

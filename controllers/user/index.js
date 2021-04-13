e17a7e1c5a9092d050f2b488535bc1c531fd251d
YOOJIYUN <ys7638@naver.com>
2021-04-13 15:28:24 +0900

    fix index.js

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,1 +1,9 @@
+const { Router } = require("express");
+const router = Router();
+const ctrl = require("./user.ctrl");
 
+router.post("/register", ctrl.register);
+router.post("/login", ctrl.login);
+router.post("/id", ctrl.findUserById);
+
+module.exports = router;

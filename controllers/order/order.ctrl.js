f48311f5ff26b9e734794bf064ae3bedc11069d9
kimjinsoo <1004kjs_@naver.com>
2021-04-13 19:28:49 +0900

    order 부분 수정

--- controllers/order/order.ctrl.js
+++ controllers/order/order.ctrl.js
@@ -1,24 +1,29 @@
 const { Goods, Order} = require("../../models");
 
+// 주문
 exports.order = async(req, res)=>{
-    const { user, goods, address_one, address_two,delivery_comment,payment_method} = req.body;
+    const { user, goods, quantity, address_one, address_two,delivery_comment,payment_method} = req.body;
     let { phone_number } = req.body
-    if(phone_number.includes("-")) {
-        console.log(phone_number.split('-'))
-        phone_number = )
+
+    order = await Order.findOne({ user : user, goods : goods})
+    if (order) {
+        await Order.updateOne({user:user, goods, goods}, {$set : {quantity : this.order.quantity + quantity}})
     }
 
-    
+    if(phone_number.includes("-")) {
+        phone_number = phone_number.split('-')
+        phone_number = phone_number.join('')
+    }
 
-
-    // await Order.create({
-    //     user             : user,
-    //     goods            : goods,
-    //     address_one      : address_one,
-    //     address_two      : address_two,
-    //     delivery_comment : delivery_comment,
-    //     phone_number     : phone_number,
-    //     payment_method   : payment_method
-    // })
-    res.status(200).send("성공")
+    await Order.create({
+        user             : user,
+        goods            : goods,
+        quantity         : quantity,
+        address_one      : address_one,
+        address_two      : address_two,
+        delivery_comment : delivery_comment,
+        phone_number     : phone_number,
+        payment_method   : payment_method
+    })
+    res.status(200).send({ result : "success" })
 }

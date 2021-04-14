e6b91d4cf829a1d9270f591ac832b615789f90e4
YOOJIYUN <ys7638@naver.com>
2021-04-14 10:54:29 +0900

    fix order and user shcema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-  id: { type: String },
-  password: { type: String },
-  name: { type: String },
+  id: { type: String, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String },
   phone_number: { type: String },
+  zipcode: { type: String },
   address_one: { type: String },
   address_two: { type: String },
 });

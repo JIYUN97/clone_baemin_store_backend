1d95cb9f7629345e341ef782459dacd73cac7474
YOOJIYUN <ys7638@naver.com>
2021-04-16 21:25:39 +0900

    fix user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
   id: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   email: { type: String },
   phone_number: { type: String },

7b955903c7b5c55ed96e131bec314c9d5f844d67
YOOJIYUN <ys7638@naver.com>
2021-04-20 15:35:10 +0900

    fix git충돌 & 주문내역 복구

--- controllers/order/order.ctrl.js
+++ controllers/order/order.ctrl.js
@@ -1,46 +1,54 @@
 const { User, Goods, Order } = require("../../models");
 
 // 주문
-exports.order_post = async(req, res)=>{
-    try {
-        const { goods, quantity, address_one, address_two,zipcode,delivery_comment,payment_method} = req.body;
-        let { phone_number } = req.body
- 
-        if(phone_number.includes("-")) {
-            phone_number = phone_number.split('-')
-            phone_number = phone_number.join('')
-        }
-        
-        await Order.create({
-            user             : res.locals.user,
-            goods            : goodsId,
-            quantity         : quantity,
-            address_one      : address_one,
-            address_two      : address_two,
-            zipcode          : zipcode, 
-            delivery_comment : delivery_comment,
-            phone_number     : phone_number,
-            payment_method   : payment_method
-        })
-        res.status(200).send({ result : "success" })
-    }catch (err) {
-        console.log(err)
-        res.status(400).send({ result : "fail"})
-    }
-}
+exports.order_post = async (req, res) => {
+  try {
+    const {
+      goods,
+      quantity,
+      address_one,
+      address_two,
+      zipcode,
+      delivery_comment,
+      payment_method,
+    } = req.body;
+    let { phone_number } = req.body;
 
-exports.order_get = async(req, res) => {
-    try{
-        const { user }  = res.locals.user
-        orders = await Order.find({ user })
-        res.status(200).send({
-            result : orders
-        })
-    }catch(err) {
-        console.log(err)
-        res.status(400).send({
-            result : "fail"
-        })
+    if (phone_number.includes("-")) {
+      phone_number = phone_number.split("-");
+      phone_number = phone_number.join("");
     }
-}
 
+    await Order.create({
+      user: res.locals.user,
+      goods: goodsId,
+      quantity: quantity,
+      address_one: address_one,
+      address_two: address_two,
+      zipcode: zipcode,
+      delivery_comment: delivery_comment,
+      phone_number: phone_number,
+      payment_method: payment_method,
+    });
+    res.status(200).send({ result: "success" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ result: "fail" });
+  }
+};
+
+//마아페이지 주문내역
+exports.getMyOrder = async (req, res) => {
+  const userId = res.locals.user;
+  try {
+    orders = await Order.find({ user: userId })
+      .populate("user", "id name")
+      .populate("goods")
+      .select("user goods quantity")
+      .sort("-createdAt");
+    res.status(200).send({ result: orders });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ err: err.message });
+  }
+};

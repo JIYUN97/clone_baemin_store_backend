472e282cfb24e2adbd69107e10dcb6c470c4fa75
YOOJIYUN <ys7638@naver.com>
2021-04-20 20:33:12 +0900

    깃 충돌 부분 복구

--- controllers/order/order.ctrl.js
+++ controllers/order/order.ctrl.js
@@ -13,16 +13,6 @@ exports.order_post = async (req, res) => {
   } = req.body;
   let { phone_number } = req.body;
 
-  order = await Order.findOne({ user: res.locals.user, goods: goods });
-  if (order) {
-    await Order.updateOne(
-      { user: res.locals.user, goods },
-      { $set: { quantity: order.quantity + quantity } }
-    );
-    res.status(200).send({ result: "success" });
-    return;
-  }
-
   if (phone_number.includes("-")) {
     phone_number = phone_number.split("-");
     phone_number = phone_number.join("");
